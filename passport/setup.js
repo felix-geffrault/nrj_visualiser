9b4c1e5b46d10cc3bd02ec6e1d7c7b4cfc713a5b
Félix <felix.geffrault@gmail.com>
2021-11-08 21:13:41 +0100

    Mise en place du système utilisateur.

--- passport/setup.js
+++ passport/setup.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/Users");
 const passport = require("passport");
+const i18next = require("i18next");
 const LocalStrategy = require("passport-local").Strategy;
+const t = i18next.t
+
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -14,47 +17,45 @@ passport.deserializeUser((id, done) => {
 });
 
 // Local Strategy
-passport.use(
-    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+passport.use("login" ,new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
         // Match User
         User.findOne({ email: email })
             .then(user => {
-                // Create new User
-                if (!user) {
-                    const newUser = new User({ email, password });
-                    // Hash password before saving in database
-                    bcrypt.genSalt(10, (err, salt) => {
-                        bcrypt.hash(newUser.password, salt, (err, hash) => {
-                            if (err) throw err;
-                            newUser.password = hash;
-                            newUser
-                                .save()
-                                .then(user => {
-                                    return done(null, user);
-                                })
-                                .catch(err => {
-                                    return done(null, false, { message: err });
-                                });
-                        });
-                    });
-                    // Return other user
-                } else {
-                    // Match password
-                    bcrypt.compare(password, user.password, (err, isMatch) => {
-                        if (err) throw err;
-
-                        if (isMatch) {
-                            return done(null, user);
-                        } else {
-                            return done(null, false, { message: "Wrong password" });
-                        }
-                    });
-                }
+                // Match password
+                if(!user) return done(null, false, {message: "invalid.credential"})
+                bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if (err) throw err;
+                    if (isMatch) return done(null, user)
+                    else return done(null, false, { message: "invalid.credential"});
+                });
             })
-            .catch(err => {
-                return done(null, false, { message: err });
-            });
+            .catch(err => done(null, false, { message: err }));
     })
 );
+
+passport.use("register", new LocalStrategy({usernameField: "email"}, (email, password, done) =>{
+    User.findOne({ email: email })
+        .then(user => {
+            if(user) return done(null, false, {message: "register.email.used"});
+            if(!/^\S+@\S+\.\S+$/.test(email)) return done(null, false, {message: "register.email.invalid"});
+            if(password.length < 6) return done(null, false, {message: "register.password.short"});
+            const newUser = new User({ email, password });
+            // Hash password before saving in database
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+                            return done(null, user);
+                        })
+                        .catch(err => {
+                            return done(null, false, { message: err });
+                        });
+                });
+            });
+        })
+}))
+
 
 module.exports = passport;
--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,27 @@ const router = express.Router();
 const passport = require("passport");
 
 router.post("/login", (req, res, next) => {
-    passport.authenticate("local", function(err, user, info) {
+    passport.authenticate("login", function(err, user, info) {
         if (err) {
             return res.status(400).json({ errors: err });
         }
         if (!user) {
-            return res.status(400).json({ errors: "No user found" });
+            return res.status(400).json({ errors: req.t(info.message) });
         }
         req.logIn(user, function(err) {
-            if (err) {
-                return res.status(400).json({ errors: err });
-            }
-            return res.status(200).json({ success: `logged in ${user.id}` });
+            if (err) return res.status(400).json({ errors: err });
+            return res.status(200).json({ success: `Logged in ${user.id}` });
+        });
+    })(req, res, next);
+});
+
+router.post("/register", (req, res, next) => {
+    passport.authenticate("register", (err, user, info) => {
+        if (err) return res.status(400).json({ errors: err });
+        if (!user) return res.status(400).json({ errors: req.t(info.message)});
+        req.logIn(user, (err) => {
+            if (err) return res.status(400).json({ errors: err });
+            return res.status(200).json({ success: `Registered with ${user.id}` });
         });
     })(req, res, next);
 });
