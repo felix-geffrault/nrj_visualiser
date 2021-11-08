9e364d6f85d77fe314e5d8ef18bb717c07c9c6ab
Félix <felix.geffrault@gmail.com>
2021-11-08 21:03:09 +0100

    Mise en place de l'internationalisation

--- app.js
+++ app.js
@@ -2,21 +2,35 @@ const express = require('express');
 const session = require('express-session');
 const MongoStore = require("connect-mongo");
 const mongoose = require('mongoose');
-
+const i18next = require('i18next');
+const Backend = require('i18next-fs-backend');
+const middleware = require('i18next-http-middleware')
 const passport = require('./passport/setup');
 const auth = require("./routes/auth");
+
 require('dotenv').config()
-
-const app = express();
-
 const MONGO_URI = process.env.DB_CONNECTION;
-console.log(MONGO_URI)
+i18next
+    .use(Backend)
+    .use(middleware.LanguageDetector)
+    .init({
+        debug: true,
+        initImmediate: false,
+        fallbackLng: 'en',
+        preload: ['en', 'fr'],
+        backend: {
+            loadPath: './locales/{{lng}}/{{ns}}.json',
+            addPath: './locales/{{lng}}/{{ns}}.json',
+        }
+    })
 
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true })
-    .then(console.log(`MongoDB connected ${MONGO_URI}`))
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(console.log(`MongoDB connected`))
     .catch(err => console.log(err));
 
+const app = express();
+app.use(middleware.handle(i18next));
 // Bodyparser middleware, extended false does not allow nested payloads
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,7 +50,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
+app.post('/locales/:lng/:ns', middleware.missingKeyHandler(i18next))
 app.use("/auth", auth);
 app.get("/", (req, res) => res.send("Good monring sunshine!"));
 
-app.listen(3000);
+app.listen(8080);
